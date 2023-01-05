2e1e27d9883cbc951f94695e37c2c4e47a1919a3
王钱龙 <wql@wangqianlong.com>
2023-01-05 22:20:43 +0800

    2023年1月5日

---
+++ ArrayListDemo/src/User.java
@@ -0,0 +1,63 @@
+public class User {
+    private String id;
+    private String username;
+    private int password;
+
+    public User() {
+    }
+
+    public User(String id, String username, int password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * 获取
+     * @return password
+     */
+    public int getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置
+     * @param password
+     */
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+}
---
+++ StudentManage/src/Student.java
@@ -0,0 +1,80 @@
+public class Student {
+    private String id;
+    private String name;
+    private int age;
+    private String adress;
+
+    public Student() {
+    }
+
+    public Student(String id, String name, int age, String adress) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.adress = adress;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return adress
+     */
+    public String getAdress() {
+        return adress;
+    }
+
+    /**
+     * 设置
+     * @param adress
+     */
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
