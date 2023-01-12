1ef4f0ac6c07899679083d65498ed2da22847362
王钱龙 <wql@wangqianlong.com>
2023-01-12 20:39:02 +0800

    2023年1月12日

---
+++ java学习/Regex/src/RegexDemo1.java
@@ -0,0 +1,33 @@
+public class RegexDemo1 {
+    /* 假如现在要求校验一个qq号码是否正确。
+            规则:6位及20位之内，0不能在开头，必须全部是数字。
+            先使用目前所学知识完成校验需求然后体验一下正则表达式检验。
+        */
+    public static void main(String[] args) {
+        String QQ = "1234567890";
+        boolean res = checkQQ(QQ);
+        System.out.println(res);
+
+        //正则表达式
+        System.out.println(QQ.matches("[1-9]\\d{5,19}"));
+    }
+    public static boolean checkQQ(String qq){
+        if (qq.length() < 6 || qq.length() >20){
+            return false;
+        }
+
+        //不能是0开头
+        if (qq.startsWith("0")){
+            return false;
+        }
+
+        //全部是数字
+        char[] arr = qq.toCharArray();
+        for (char c : arr) {
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ java学习/Regex/src/RegexTest1.java
@@ -0,0 +1,25 @@
+public class RegexTest1 {
+    /*
+            需求
+            请编写正则表达式验证用户输入的手机号码是否满足要求。请编写正则表达式验证用户输入的邮箱号是否满足要求。请编写正则表达式验证用户输入的电话号码是否满足要求。
+            验证手机号码 13112345678 13712345667 13945679027 139456790271
+            验证座机电话号码 020-2324242 02122442 027-42424 0712-3242434
+            验证邮箱号码 3232323@qq.com zhangsan@itcast.cnn dlei0009@163.com dlei0009@pci.com.cn
+        */
+    public static void main(String[] args) {
+        String phone = "13112345678";
+        boolean res = phone.matches("[1][3-9]\\d{9}");
+        System.out.println(res);
+
+        String homePhone = "020-2324242";
+        boolean res1 = homePhone.matches("0\\d{2,3}-?[1-9]\\d{4,9}");
+        System.out.println(res1);
+
+        String email = "3232323@qq.com";
+        String email1 = "dlei0009@pci.com.cn";
+        boolean res2 = email.matches("\\w+@[\\w&&[^_]]{2,6}(.[a-zA-Z]{2,3}){1,2}");
+        boolean res3 = email1.matches("\\w+@[\\w&&[^_]]{2,6}(.[a-zA-Z]{2,3}){1,2}");
+        System.out.println(res2);
+        System.out.println(res3);
+    }
+}
